69cafc40ec1900690d63f01d6fd809ee8041fa57
Harsh Shinde <harshshinde0608@gmail.com>
2026-01-15 17:49:42 +0530

    Time: 12 ms (7.62%), Space: 46.8 MB (9.96%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        String answer="";
+        int count =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')') count--;
+            if(count!=0){
+                answer+=s.charAt(i);
+            }
+            if(s.charAt(i)=='(')count++;
+        }
+        return answer;
+    }
+}
